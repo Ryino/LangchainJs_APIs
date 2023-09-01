28eccea4d6c35b2223c9988752037ffda141fc86
Ryino <94173229+Ryino@users.noreply.github.com>
2023-09-01 12:46:38 +0500

    Update 4-deletePinecone.js

--- 4-deletePinecone.js
+++ 4-deletePinecone.js
@@ -8,11 +8,8 @@ export const deletePinecone = async (indexName, docs, userId, client) => {
     const resp = await index.delete1({
       ids: [
         `${userId}`,
-        // 1,
-        // '3_book_2',
-        // "C:\\Users\\USER\\Downloads\\Get-Started-With-Langchain-and-Pinecone-in-Node.js-main\\Get-Started-With-Langchain-and-Pinecone-in-Node.js-main\\documents\\The Great Gatsby.txt_6",
       ],
-      // deleteAll: true
+      
     });
     console.log(resp, "deleted");
   } catch (error) {
